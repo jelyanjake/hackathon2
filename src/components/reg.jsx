94b3e53a92843d0403ae9e1218e9922df3b7d2c7
jelyanjake <jelyanjake@gmail.com>
2025-04-21 12:40:52 +0800

    niggasinparis

--- src/components/reg.jsx
+++ src/components/reg.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState,useEffect, useRef } from 'react';
 import StatusModal from './StatusModal';
 
 function RegPage() {
@@ -6,11 +6,33 @@ function RegPage() {
   const [status, setStatus] = useState(null);
   const [statusMessage, setStatusMessage] = useState('');
 
+  const videoRef = useRef(null)
+
+  useEffect(() => {
+    const startCamera = async () => {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: true })
+        if (videoRef.current) {
+          videoRef.current.srcObject = stream
+        }
+      } catch (err) {
+        console.error("Error accessing camera:", err)
+      }
+    }
+
+    startCamera()
+  }, [])
+
   return (
     <section id="features">
       <div className="container">
         <div className="section-title">
           <h2>lokhhhhhh</h2>
+          <div className='videos'>
+          <video ref={videoRef} autoPlay playsInline width="1080" height="720" />
+          </div>
+          <div className='videos'>
+          </div>
           <p>awdawd</p>
         </div>
       </div>
